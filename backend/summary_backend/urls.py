44aa6036ac97e51e22e93822edcb3761425884b1
Forhath Hosain Ome <omehasan3@gmail.com>
2026-04-22 00:55:03 +0600

    urls added for top-5

--- backend/summary_backend/urls.py
+++ backend/summary_backend/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("puma/", include("puma_summary.urls", namespace="puma_summary")),
     path("image/", include("image_processor.urls", namespace="image_processor")),
     path("final-summary/", include("final_summary.urls", namespace="final_summary")),
+    path("top-five/", include("top_five.urls", namespace="top_five")),
 ]
