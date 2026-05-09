4e20c134b675556b9e9d1d17946a79133993c4f7
Forhath Hosain Ome <omehasan3@gmail.com>
2026-05-09 13:07:28 +0600

    base model/'s created_by fixed.

--- backend/shared/models/base_model.py
+++ backend/shared/models/base_model.py
@@ -7,7 +7,7 @@ class BaseModel(models.Model):
     """
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="defect_entries",)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="%(class)s_created")
 
     class Meta:
         abstract = True
