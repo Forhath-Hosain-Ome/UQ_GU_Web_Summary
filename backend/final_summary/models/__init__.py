ae2a000c10a1ff34e3219fbc49c0c6bf29be70b9
Forhath Hosain Ome <omehasan3@gmail.com>
2026-05-09 13:03:06 +0600

    Models Fixed with requirements

--- backend/final_summary/models/__init__.py
+++ backend/final_summary/models/__init__.py
@@ -1,5 +1,5 @@
 from .audit_report import AuditReport
-from .buyer_factory_pair import BuyerFactoryPair
+from .buyer_factory_pair import BuyerFactoryPair, ReportType, AvailableReport
 from .buyer import Buyer
 from .defect_entry import DefectEntry
 from .factory import Factory
